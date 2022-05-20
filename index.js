37693b0e40637f0d3738eac6a2a039036eadc32f
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-21 04:45:52 +0600

    Load user data to db implemented

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services')
         const bookingCollection = client.db('doctors_portal').collection('bookings')
+        const userCollection = client.db('doctors_portal').collection('users')
 
 
 
@@ -28,6 +29,18 @@ async function run() {
             const cursor = serviceCollection.find(query)
             const services = await cursor.toArray()
             res.send(services)
+        })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email: email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            res.send(result)
         })
 
         // Warning:
@@ -63,6 +76,7 @@ async function run() {
         * app.get('/booking/:id') // get a specific booking
         * app.post('/booking') // add a new book
         * app.patch('/booking/:id') //
+        * app.put('/booking/:id') // upsert ==> update(if exists) or insert (if does't exist)
         * app.delete('/booking/:id') //
         */
 
